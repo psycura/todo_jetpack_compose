1a23c4aa2187e98b8423ff4a6935a869cfb7769a
Eugene <psycura@gmail.com>
2024-06-27 17:12:53 +0300

    start to work on dropdown

---
+++ app/src/main/java/com/example/to_docompose/components/PriorityDropdown.kt
@@ -0,0 +1,88 @@
+package com.example.to_docompose.components
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.to_docompose.R
+import com.example.to_docompose.data.models.Priority
+import com.example.to_docompose.ui.theme.MEDIUM_PADDING
+import com.example.to_docompose.ui.theme.PRIORITY_DROP_DOWN_HEIGHT
+import com.example.to_docompose.ui.theme.PRIORITY_INDICATOR_SIZE
+
+@Composable
+fun PriorityDropdown(
+    priority: Priority,
+    onPrioritySelected: (Priority) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(PRIORITY_DROP_DOWN_HEIGHT)
+            .clickable { expanded = !expanded }
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f),
+                shape = MaterialTheme.shapes.small
+            ),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Canvas(
+            modifier = Modifier.size(PRIORITY_INDICATOR_SIZE)
+                .weight(1f)
+        ) {
+            drawCircle(color = priority.color)
+        }
+        Text(
+            modifier = Modifier.padding(start = MEDIUM_PADDING)
+                .weight(8f),
+            text = priority.name,
+            style = MaterialTheme.typography.bodyMedium,
+        )
+        IconButton(
+            onClick = { expanded = !expanded },
+            modifier = Modifier
+                .alpha(alpha = 0.50f)
+                .rotate(0f)
+                .weight(1.5f)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.ArrowDropDown,
+                contentDescription = stringResource(R.string.drop_down_arrow)
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun PriorityDropdownPreview() {
+    PriorityDropdown(
+        priority = Priority.LOW,
+        onPrioritySelected = {}
+    )
+}
